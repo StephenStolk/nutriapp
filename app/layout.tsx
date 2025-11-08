9818a10feb95bb48f9e7f619a3ad3e9c66ae45ad
StephenStolk <shikharburman2@gmail.com>
2025-11-08 15:06:12 +0530

    fixxed bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { Suspense } from "react"
 import { ThemeProvider } from "@/components/theme-provider"
 
